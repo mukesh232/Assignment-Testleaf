e32716e99292677257f9219cca2d64e38bdfa882
Mukesh M S <mukeshaaron.38@gmail.com>
2025-10-10 13:22:41 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,21 @@
+package week1day3;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		 float version = 91.0f;
+	        String developer = "Google";
+	        boolean isBeta = false;
+	        int releaseYear = 2008;
+	        char shortcutKey = 'C';
+	        System.out.println("Version: " + version);
+	        System.out.println("Developer: " + developer);
+	        System.out.println("Is Beta: " + isBeta);
+	        System.out.println("Release Year: " + releaseYear);
+	        System.out.println("Shortcut Key: " + shortcutKey);
+	    }
+	
+
+	}
+
+
