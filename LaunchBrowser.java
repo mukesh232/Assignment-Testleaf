c5f4b5a448cafb30eebf30bbcac02c8c12b29acc
Mukesh M S <mukeshaaron.38@gmail.com>
2025-10-26 09:39:20 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,26 @@
+package automation_projects_dummy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		Thread.sleep(1000);
+
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.tagName("button")).click();
+		driver.findElement(By.partialLinkText("PIM")).click();
+
+		 // close browser properly
+	}
+}
