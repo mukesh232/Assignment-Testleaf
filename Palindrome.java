6927c2b5cebe46c040f7a145549ef409abffd9ab
Mukesh M S <mukeshaaron.38@gmail.com>
2025-10-28 21:05:32 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+package javaTest;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String word = "14321";
+		String word2= "";
+		for(int i= word.length()-1; i>=0;i--)
+		{
+			
+			word2=word2 + word.charAt(i);
+		}
+		System.out.println(word2);
+		if(word.equals(word2))
+			System.out.println("palindrome");
+		else System.out.println("Not palindrome");
+	}
+
+}
